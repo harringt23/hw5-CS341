75c28e70590209416f67ed3af73389a83d90f3fc
BH <74618590+harringt23@users.noreply.github.com>
2022-02-19 16:40:30 -0800

    was missing including i think?

--- hw5/public/javascripts/arrow_handler.js
+++ hw5/public/javascripts/arrow_handler.js
@@ -1,16 +1,16 @@
-/* orders_placed.js
+/* arrow_handler.js
  * Homework 5 
  * @author Brynn Harrington
  * Date Modified: 10 Feb 2022
  * 
- * This is the handler for the forms
+ * This is the handler for the arrow
  */
 
 arrow_change_handler = function(e) {
-    $("#testimonial-arrow").toggleClass("mirrored");
+    $("#onial-arrow").toggleClass("mirrored");
 }
 
 $(function() {
     // If the testamonial card is "mouseover'd" or "mouseout'd", toggle the direction of the arrow.
-    $("#testimonials").mouseover(arrow_change_handler).mouseout(arrow_change_handler);
+    $("#onials").mouseover(arrow_change_handler).mouseout(arrow_change_handler);
 });
